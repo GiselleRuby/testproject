0d803c2ead67a1d1fbd959bc2e011d39ea9067e1
blackwindsky <blackwindsky@gmail.com>
2014-03-01 17:45:26 +0800

    practice for Chater 3

---
+++ text.rb
@@ -0,0 +1,23 @@
+puts 'This a test'
+puts ''
+puts 'thirth line'
+
+puts 'I love the '+'apple pie'
+
+puts 'No, ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+# puts '12' * '12'
+# puts '12' + 12
+
+puts 'You\'re swell'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
