2abbaab26a80b64dfa2750960552797af25ff95f
blackwindsky <blackwindsky@gmail.com>
2014-03-01 22:10:24 +0800

    practice for Chapter 4

---
+++ variabled.rb
@@ -0,0 +1,10 @@
+name = 'Anya Christina Emmanuella Jenkins Harris'
+age = 20
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is a really long name!'
+
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+my_own_var = 5 * (1+2)
+puts my_own_var
